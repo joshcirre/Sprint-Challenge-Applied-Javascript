886fdd28fd57ec4a142d67bebd166ad2df3f5d76
Josh Cirre <joshcirre@gmail.com>
2019-10-12 09:33:13 -0700

    added a component for Cards feature pulling in data from backend :sparkles:

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,73 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// Pull articles from Backend
+const getArticles = callback => {
+  axios
+    .get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then(response => callback(response))
+    .catch(error => console.log(error));
+};
+
+// Inspect Data
+const logData = ({ data }) => {
+  console.log(data.articles);
+};
+
+// Set-up DOM Elements
+const cardContainer = (data, topic) => {
+  articlesContainer = document.querySelector('.cards-container');
+  data.articles[`${topic}`].forEach(article => {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('data-topic', `${topic}`);
+
+    let headline = document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent = article.headline;
+    card.appendChild(headline);
+
+    let author = document.createElement('div');
+    author.classList.add('author');
+    card.appendChild(author);
+    let authorImgContainer = document.createElement('div');
+    authorImgContainer.classList.add('img-container');
+    author.appendChild(authorImgContainer);
+    let authorPhoto = document.createElement('img');
+    authorPhoto.src = article.authorPhoto;
+    authorImgContainer.appendChild(authorPhoto);
+    let authorName = document.createElement('span');
+    authorName.textContent = `By ${article.authorName}`;
+    author.appendChild(authorName);
+
+    articlesContainer.appendChild(card);
+  });
+};
+
+const bootstrapArticles = ({ data }) => {
+  cardContainer(data, 'bootstrap');
+};
+
+const javascriptArticles = ({ data }) => {
+  cardContainer(data, 'javascript');
+};
+
+const jqueryArticles = ({ data }) => {
+  cardContainer(data, 'jquery');
+};
+
+const nodeArticles = ({ data }) => {
+  cardContainer(data, 'node');
+};
+
+const technologyArticles = ({ data }) => {
+  cardContainer(data, 'technology');
+};
+
+getArticles(logData);
+getArticles(bootstrapArticles);
+getArticles(javascriptArticles);
+getArticles(jqueryArticles);
+getArticles(nodeArticles);
+getArticles(technologyArticles);
